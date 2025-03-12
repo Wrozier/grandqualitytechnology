28f5c36ea84a541cc0ccb080b3d8323f2f768eb3
William Rozier <wrozier@princesscruises.com>
2025-03-12 11:46:33 -0400

    added social links

--- components/Socials.js
+++ components/Socials.js
@@ -14,13 +14,13 @@ import {
 const Socials = () => {
   return (
     <div className='flex items-center gap-x-5 text-lg'>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.linkedin.com/in/william-rozier-900902100/'} className='hover:text-accent transition-all duration-300'>
         <RiYoutubeLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://github.com/Wrozier?tab=repositories'} className='hover:text-accent transition-all duration-300'>
         <RiFacebookLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.instagram.com/gqdevops?igsh=MTVqOWdtaHJva3Yzbw%3D%3D&utm_source=qr'} className='hover:text-accent transition-all duration-300'>
         <RiInstagramLine />
       </Link>
       <Link href={''} className='hover:text-accent transition-all duration-300'>
